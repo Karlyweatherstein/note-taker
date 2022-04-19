a905289ddf9c08f429d7d0f0fde927436a341d2e
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-04-18 21:27:31 -0400

    adding the port and paths to other files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+
+//creating paths
+const dbPath = path.join(__dirname, "/db")
+const publicPath = path.join(__dirname, "/public")
+
+// Running on PORT 3001
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// app.get('/')
+
+// Listening
+app.listen(PORT, () => {
+    console.log(`API server now on port 3001`);
+});
